2154a42446e715d7ee5343da51dfcc9135e97f63
De la Cruz Tovar <awsgus@784f4353ee33.ant.amazon.com>
2018-10-26 18:24:54 -0500

    Manejo de exit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 use std::io;
 use std::io::Write;
 use std::process::Command;
-use  std::str::from_utf8;
 
-
+fn parser(input : &String ) -> Result < Vec<&str> , String  > {
+    let mut args: Vec<&str> = input.split(' ').collect();
+    let command=args[0];
+    if command == "exit" {
+      Err("Mr Stark I don´t want to die :/".to_string())
+    } else {
+      Ok(args)
+    }
+}
+ 
 fn main() {
   loop {
     println!("gush! ");
@@ -12,20 +20,25 @@ fn main() {
     io::stdin().read_line(&mut input);
     let len=input.len()-1;
     input.truncate(len);
-    let mut args: Vec<&str> = input.split(' ').collect();
 
-    let command=args.remove(0);
-    let output=Command::new(&command)
+    if ! input.is_empty() {
+        let mut args =  match parser(&input) {
+          Ok(a) => { println!("{:?}",a);  a } ,
+          Err(e) => { println!("{}",e); break; } ,
+        };
+        let command=args.remove(0);
+        let output=Command::new(&command)
             .args(&args)
             .output()
             .expect("Lee la documentacion, animal");
     
-    let stdout = io::stdout();
-    let mut handle = stdout.lock();
-    handle.write( &output.stdout );
+        let stdout = io::stdout();
+        let mut handle = stdout.lock();
+        handle.write( &output.stdout );
 
-    let stderr = io::stderr();
-    let mut handle = stderr.lock();
-    handle.write( &output.stderr );
+        let stderr = io::stderr();
+        let mut handle = stderr.lock();
+        handle.write( &output.stderr );
+   } //if
   }
 }
