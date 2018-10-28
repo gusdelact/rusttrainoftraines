dc79b91e760a7707500d746a105a782d55a64e9d
De la Cruz Tovar <awsgus@784f4353ee33.ant.amazon.com>
2018-10-28 01:08:48 -0500

    una primera implantacion de pipe

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
 use std::io;
 use std::io::Write;
+use std::io::Read;
+use std::error::Error;   
 use std::process::Command;
-
+use std::process::Stdio;
 #[allow(dead_code)]
 #[derive(Debug)]
 struct Comando<'a> {
     binario: String,
     argumentos: Vec<&'a str>,
-    
-}
+}  //Comando
 
-fn parserv2(comandos : &String ) ->  Vec<Comando>  {
+fn parserv2 (comandos :  &String , separador: String  ) -> Vec<Comando >  {
     let mut lote : Vec<Comando> = Vec::new();
-    let commands: Vec<&str> = comandos.split(';').collect();
+    let commands: Vec<&str> = comandos.split(&separador).collect();
     
     for c in commands {
          let mut args : Vec<&str> = c.split_whitespace().collect();
@@ -22,20 +23,30 @@ fn parserv2(comandos : &String ) ->  Vec<Comando>  {
              argumentos: args,
          };
          lote.push(comando);
-         
     }
     lote
 }
 
 #[test]
-fn test_parserv2() {
+fn test_parserv2_sequential() {
    let comandos = "ls -l; pwd ; ps -fea ".to_string();
-   let lote =parserv2(&comandos);
+   let lote =parserv2(&comandos, ";".to_string());
    assert!(lote.len() == 3);
    assert!(lote[0].binario == "ls" );
    assert!(lote[0].argumentos[0] == "-l" );
    assert!(lote[2].binario == "ps" );
    assert!(lote[2].argumentos[0] == "-fea" );
+}
+
+#[test]
+fn test_parserv2_pipe() {
+   let comandos = "ls -l  | wc -l ".to_string();
+   let lote =parserv2(&comandos, "|".to_string());
+   assert!(lote.len() == 2);
+   assert!(lote[0].binario == "ls" );
+   assert!(lote[0].argumentos[0] == "-l" );
+   assert!(lote[1].binario == "wc" );
+   assert!(lote[1].argumentos[0] == "-l" );
 }
 
 fn ejecutar_lote(lote: &Vec<Comando> ) {
@@ -76,9 +87,51 @@ fn test_ejecutar_lote_tres_comandos( ) {
   ejecutar_lote(&lote);
 } //test_ejecutar_lote_tres_comandos
 
+fn ejecutar_pipe(pipe: &Vec<Comando> ) {
+        let comando= &pipe[0];
+        let  child=Command::new(&comando.binario)
+            .args(&comando.argumentos)
+            .stdout(Stdio::piped())
+            .spawn()
+            .expect("Lee la documentacion, animal");
+        let output= child.wait_with_output().expect("Failed to wait child");
+        let salida= output.stdout;
+//arrancar siguiente comando de la tuberia
+        let comando_despues= &pipe[1];
+        let  child_despues=match Command::new(&comando_despues.binario)
+            .args(&comando_despues.argumentos)
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn() {
+          Err(why) => panic!("couldn't spawn wc: {}", why.description()),
+          Ok(child_despues) => child_despues,
+        };
+//tomar entrada estandar del siguiente comando
+        match child_despues.stdin.unwrap().write_all(&salida) {
+        Err(why) => panic!("couldn't write to wc stdin: {}",
+                           why.description()),
+           Ok(_) => println!("sent to wc"),
+        }
+        let mut s = String::new();
+       match child_despues.stdout.unwrap().read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read wc stdout: {}",
+                           why.description()),
+        Ok(_) => print!("wc responded with:\n{}", s),
+       }
+} //ejecutar_pipe
+
+#[test]
+fn test_ejecutar_pipe( ) {
+  let pipe : Vec<Comando> = vec![ 
+     Comando { binario: "ls".to_string(), argumentos: vec!["-l"] }, 
+     Comando { binario: "wc".to_string(), argumentos: vec!["-l"] },
+  ];
+  ejecutar_pipe(&pipe);
+} //test_ejecutar_lote_un_comando 
+
 fn main() {
   loop {
-    println!("gush! ");
+    print!("gush! ");
 
     let mut input = String::new();
     io::stdin().read_line(&mut input);
@@ -86,7 +139,7 @@ fn main() {
     input.truncate(len);
 
     if ! input.is_empty() {
-        let  lote =   parserv2(&input) ;
+        let  lote =   parserv2(&input,";".to_string()) ;
         ejecutar_lote(&lote);
    } //if
   }
